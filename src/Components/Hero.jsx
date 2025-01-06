4ba10ac4cc0e8e2d6c5b4e050f8aa593e4e28a05
bhuwanoj_ <bhuwanojha10@gmail.com>
2025-01-06 20:47:42 +0545

    hero slider

---
+++ src/Components/Hero.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import HeroImg from "../assets/img/hero/bg.png";
+import Slider from "./Slider";
+
+const Hero = () => {
+  return (
+    <div className="relative">
+      <Slider />
+    </div>
+  );
+};
+
+export default Hero;
