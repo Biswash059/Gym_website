9768bc06dfeb0bef3c6014d105c356b0ddfcfbdd
bhuwanoj_ <bhuwanojha10@gmail.com>
2025-01-07 10:48:23 +0545

    hero

--- src/Components/Slider.jsx
+++ src/Components/Slider.jsx
@@ -1,76 +1,33 @@
-import React from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
-import { Navigation, Pagination, Autoplay } from "swiper/modules";
-import HeroImg from "../assets/img/hero/bg.png";
+
+// swiper styles
 import "swiper/css";
 import "swiper/css/navigation";
-import "swiper/css/pagination";
+import CustomButton from "./CustomButton";
+import SwiperButton from "./SwiperButton";
 
 const Slider = () => {
   return (
-    <section
-      className="relative w-full h-screen bg-cover bg-center"
-      style={{
-        backgroundImage: `url(${HeroImg})`,
-      }}
-    >
-      {/* Slider Container */}
-      <div className="absolute inset-0 flex items-center justify-center">
-        <div className="w-11/12 max-w-[1430px]">
-          <Swiper
-            modules={[Navigation, Pagination, Autoplay]}
-            loop={true}
-            navigation={true}
-            pagination={{ clickable: true }}
-            autoplay={{ delay: 4000 }}
-            className="swiper-container"
-          >
-            <SwiperSlide>
-              <div className=" p-6 text-right shadow-md rounded">
-                <h1 className="font-bold text-6xl text-red-600">HARD WORK</h1>
-                <h3 className="font-bold text-6xl text-white">
-                  IS FOR EVERY SUCCESS
-                </h3>
-                <p className="text-white m-2">
-                  start by taking inspiration,continue it to give inspiration
-                </p>
-                <button className=" mt-9 ml-10 py-4 px-3 border border-red-600 rounded-lg bg-red-600 text-white ">
-                  GET STARTED
-                </button>
-              </div>
-            </SwiperSlide>
-            <SwiperSlide>
-              <div className=" p-6 text-right shadow-md rounded">
-                <h1 className="font-bold text-6xl text-red-600">HARD WORK</h1>
-                <h3 className="font-bold text-6xl text-white">
-                  IS FOR EVERY SUCCESS
-                </h3>
-                <p className="text-white m-2">
-                  start by taking inspiration,continue it to give inspiration
-                </p>
-                <button className=" mt-9 ml-10 py-4 px-3 border border-red-600 rounded-lg bg-red-600 text-white ">
-                  GET STARTED
-                </button>
-              </div>
-            </SwiperSlide>
-            <SwiperSlide>
-              <div className=" p-6 text-right shadow-md rounded">
-                <h1 className="font-bold text-6xl text-red-600">HARD WORK</h1>
-                <h3 className="font-bold text-6xl text-white">
-                  IS FOR EVERY SUCCESS
-                </h3>
-                <p className="text-white m-2">
-                  start by taking inspiration,continue it to give inspiration
-                </p>
-                <button className=" mt-9 ml-10 py-4 px-3 border border-red-600 rounded-lg bg-red-600 text-white ">
-                  GET STARTED
-                </button>
-              </div>
-            </SwiperSlide>
-          </Swiper>
+    <Swiper>
+      <SwiperSlide>
+        <div className="h-full flex justify-end pt-48">
+          <div className="flex flex-col items-center lg:items-start lg:max-w-[700px]">
+            <h1 className="text-6xl text-white font-bold text-center lg:text-left mb-4">
+              <span>Hard Work </span>
+              is For Every Success
+            </h1>
+            <p className="text-white italic text-center lg:text-left mb-4">
+              Start by taking inspiration, Continue it to give inspiration
+            </p>
+            <CustomButton
+              text="Get started"
+              containerStyles="w-[196px] h-[62px] bg-red-300"
+            />
+          </div>
         </div>
-      </div>
-    </section>
+      </SwiperSlide>
+      <SwiperButton />
+    </Swiper>
   );
 };
 
