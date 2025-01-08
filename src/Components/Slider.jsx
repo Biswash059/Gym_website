d72b16af4970e48259d215caadafd2844f65bd59
bhuwanoj_ <bhuwanojha10@gmail.com>
2025-01-08 12:56:14 +0545

    nothing done

--- src/Components/Slider.jsx
+++ src/Components/Slider.jsx
@@ -9,14 +9,12 @@ import SwiperButton from "./SwiperButton";
 const Slider = () => {
   return (
     <div className="relative w-full h-full">
-      <Swiper className="relative z-10">
-        <SwiperSlide className="relative">
+      <Swiper>
+        <SwiperSlide>
           <div className=" h-full flex justify-end pt-48">
             <div className="flex flex-col items-center lg:items-start lg:max-w-[700px]">
-              <h1 className="text-6xl text-white font-bold text-center lg:text-left mb-4 sm:px-9">
-                <span>
-                  Hard Work <br />
-                </span>
+              <h1 className="text-6xl text-white font-bold text-center lg:text-left mb-4 ">
+                <span>Hard Work</span> <br />
                 is For Every Success
               </h1>
               <p className="text-white text-center lg:text-left mb-4">
