4d06deb45eee5537e2e38404b8e195fe572c6590
Biswash <koiralabiswash123@gmail.com>
2025-01-08 14:46:19 +0545

    Classes section added

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import Header from "../Components/Header";
 import Hero from "../Components/Hero";
 import AboutSection from "../Components/AboutSection";
+import Classes from "../Components/Classes";
 
 const Home = () => {
   return (
     <>
       <Hero />
       <AboutSection />
+      <Classes />
     </>
   );
 };
